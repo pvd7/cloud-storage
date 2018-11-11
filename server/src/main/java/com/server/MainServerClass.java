65e1393dee44555b9073f0cdea025c1d45aac824
Pavel <pvdymov@ya.ru>
2018-11-11 22:22:29 +0700

    update

--- server/src/main/java/com/server/MainServerClass.java
+++ server/src/main/java/com/server/MainServerClass.java
@@ -1,10 +1,8 @@
 package com.server;
 
-import com.common.CommonCat;
-
 public class MainServerClass {
-    public static void main(String[] args) {
-        CommonCat cat = new CommonCat("Barsik");
 
+    public static void main(String[] args) {
     }
+
 }
