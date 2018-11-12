73b322aaa75a11edfa5de8f27cd7948dc011e52c
Pavel Dymov <pvdymov@vankoroil.ru>
2018-11-12 17:19:31 +0700

    upd Client

---
+++ client/src/main/java/com/client/controller/Controller.java
@@ -0,0 +1,84 @@
+package com.client.controller;
+
+import com.client.net.Network;
+import com.common.entity.AbstractMessage;
+import com.common.entity.FileMessage;
+import com.common.entity.FileRequest;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    final static String STORAGE = "client_storage";
+
+    @FXML
+    TextField tfFileName;
+
+    @FXML
+    ListView<String> filesList;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Network.start();
+        Thread t = new Thread(() -> {
+            try {
+                while (true) {
+                    AbstractMessage am = Network.readObject();
+                    if (am instanceof FileMessage) {
+                        FileMessage fm = (FileMessage) am;
+                        Files.write(Paths.get( STORAGE + "/" + fm.getFilename()), fm.getData(), StandardOpenOption.CREATE);
+                        refreshLocalFilesList();
+                    }
+                }
+            } catch (ClassNotFoundException | IOException e) {
+                e.printStackTrace();
+            } finally {
+                Network.stop();
+            }
+        });
+        t.setDaemon(true);
+        t.start();
+        filesList.setItems(FXCollections.observableArrayList());
+        refreshLocalFilesList();
+    }
+
+    public void pressOnDownloadBtn(ActionEvent actionEvent) {
+        if (tfFileName.getLength() > 0) {
+            Network.sendMsg(new FileRequest(tfFileName.getText()));
+            tfFileName.clear();
+        }
+    }
+
+    public void refreshLocalFilesList() {
+        if (Platform.isFxApplicationThread()) {
+            try {
+                filesList.getItems().clear();
+                Files.list(Paths.get(STORAGE)).map(p -> p.getFileName().toString()).forEach(o -> filesList.getItems().add(o));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Platform.runLater(() -> {
+                try {
+                    filesList.getItems().clear();
+                    Files.list(Paths.get(STORAGE)).map(p -> p.getFileName().toString()).forEach(o -> filesList.getItems().add(o));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+}
+
