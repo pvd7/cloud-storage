4b394590e6abd1cb991cdf2e2ba460ca9542e366
Pavel Dymov <pvdymov@vankoroil.ru>
2018-11-22 17:03:08 +0700

    + FileUtil.exist()

---
+++ example/src/main/java/com/file/util/FileUtil.java
@@ -0,0 +1,21 @@
+package com.file.util;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileUtil {
+
+    public static String find(String[] paths, String file) throws FileNotFoundException {
+        if ("".equals(file)) throw new FileNotFoundException("the filename is empty");
+
+        int len = paths.length;
+        for (int i = 0; i < len; i++) {
+            if (Files.exists(Paths.get(paths[i] + file)))
+                return paths[i] + file;
+        }
+
+        throw new FileNotFoundException(file);
+    }
+
+}
