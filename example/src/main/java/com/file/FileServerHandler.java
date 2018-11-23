93efb741c7a30831199b37391d72b5d65ef54901
Pavel Dymov <pvdymov@vankoroil.ru>
2018-11-23 12:17:22 +0700

    upd HELLO:

--- example/src/main/java/com/file/FileServerHandler.java
+++ example/src/main/java/com/file/FileServerHandler.java
@@ -16,7 +16,8 @@ public class FileServerHandler extends SimpleChannelInboundHandler<String> {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
-        ctx.writeAndFlush("HELLO: Type the name of the file to retrieve.\n");
+        ctx.writeAndFlush("HELLO: Type the name of the file to retrieve (please enter: get:<filename>).\n");
+
     }
 
     @Override
