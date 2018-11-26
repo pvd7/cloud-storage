e3a113e7a66318854e227dd35329ccf01373780a
Pavel Dymov <pvdymov@vankoroil.ru>
2018-11-26 17:33:48 +0700

    upd FileHandler

--- server/src/main/java/com/server/handler/FileHandler.java
+++ server/src/main/java/com/server/handler/FileHandler.java
@@ -4,6 +4,7 @@ import com.common.entity.FileMessage;
 import com.common.entity.FileRequest;
 import com.server.Server;
 import com.server.util.FileUtil;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.DefaultFileRegion;
@@ -52,49 +53,23 @@ public class FileHandler extends ChannelInboundHandlerAdapter {
                     fileMessage.setRead(read);
                     fileMessage.setData(buf);
                     ctx.writeAndFlush(fileMessage);
-                    ctx.flush();
                 }
             }
 //            ctx.flush();
         } catch (IOException e) {
             ctx.writeAndFlush(e);
         }
-
-//        String path = FileUtil.find(Server.PARTS, msg.getFilename());
-//        if (Files.exists(Paths.get(STORAGE + "/" + msg.getFilename()))) {
-//            FileMessage fm = new FileMessage(Paths.get(STORAGE + "/" + msg.getFilename()));
-//            ctx.writeAndFlush(fm);
-//        }
     }
 
-    private void chanelFileWrite1(ChannelHandlerContext ctx, FileRequest msg) throws IOException {
-        String path = STORAGE + "/" + msg.getFilename();
-        if (Files.exists(Paths.get(path))) {
-//            FileMessage fm = new FileMessage(Paths.get(path));
-            RandomAccessFile file = new RandomAccessFile(path, "r");
-            ctx.write(new DefaultFileRegion(file.getChannel(), 0, file.length()));
-            ctx.writeAndFlush("\n");
-        }
-    }
-
-    /**
-     * Сохраняет файл из сообщения на диск
-     *
-     * @param msg сообщение с файлом
-     * @throws IOException исключение
-     */
     private void channelFileRead(FileMessage msg) throws IOException {
-//        Files.write(Paths.get(STORAGE + "/" + msg.getFilename()), msg.getData(), StandardOpenOption.CREATE);
-    }
-
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        ctx.flush();
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        ctx.close();
+        cause.printStackTrace();
+        if (ctx.channel().isActive()) {
+            ctx.writeAndFlush(cause).addListener(ChannelFutureListener.CLOSE);
+        }
     }
 
 }
